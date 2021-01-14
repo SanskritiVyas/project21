5b0e8edfedbee949c265ee09ad433f40c8d7b32c
SanskritiVyas <76947177+SanskritiVyas@users.noreply.github.com>
2021-01-14 15:47:31 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+var surface1,surface2,surface3,surface4,box
+surface1.shapeColor=blue;
+surface2.shapeColor=orange;
+surface3.shapeColor=pink;
+surface4.shapeColor=green;
+box.shapeColor=gren;
+var canvas;
+var music;
+
+function preload(){
+    music = loadSound("music.mp3");
+}
+
+
+function setup(){
+    canvas = createCanvas(800,600);
+
+    //create 4 different surfaces
+createSprite(random(20,750)
+if(surface1.isTouching(box) && box.bounceOff(surface1)){
+    //change color here
+}
+
+if(block2.isTouching(ball)){
+ball.shapeColor=rgb(255,128,0);
+ball.velocityX=0
+music.stop();
+}
+
+
+    //create box sprite and give velocity
+
+}
+
+function draw() {
+    background(rgb(169,169,169));
+    //create edgeSprite
+
+
+
+    //add condition to check if box touching surface and make it box
+}
